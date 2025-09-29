9b2f50bfb3116e268ebfae5a0ac1a4dd0bc0debd
Alex3336 <alexaamogus@gmail.com>
2025-09-29 17:09:42 +0300

    12121

--- lesson 13/js/script.js
+++
@@ -1,9 +0,0 @@
-let x = function (a){
-    if (parseInt(a) % 2 === 0){
-        console.log(true);
-    }
-    else{
-        console.log(false);
-    }
-}
-let venom = x(prompt("Please enter your number"));
--- lesson 32 template/js/main.js
+++
@@ -1,8 +0,0 @@
-import {countries} from './array.js';
-
-const templateSource = document.querySelector('#country-card-template').innerHTML;
-const template = Handlebars.compile(templateSource);
-const container = document.querySelector('#countries-container');
-
-const markup = template(countries);
-container.innerHTML = markup;
